1391bc591e288fb951e14a2a9edae48064d5e749
vincenttommi <vincenttommi@gmail.com>
2023-03-20 08:17:35 +0300

    control flow assigment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let feedback;
+  if (feet <= 400) {
+    feedback = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    feedback = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    feedback = 'No can do.';
+  } else {
+    feedback = 'This is an invalid distance.';
+  }
+  return feedback;
 }
 
-function ternaryCheckCity(){
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    
+ switch(tip){
+    case 'generous':
+    return "Thank you so much.";
+    break;
+    case 'not as generous':
+    return "Thank you.";
+    default:
+      return "Bye.";
+
+}
 }
